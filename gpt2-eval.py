92150d703f265b11bbc616656b367f8e378bde6e
Hannah Lin <hylin.hannah@gmail.com>
2024-02-26 00:10:29 +0000

    More fixes

--- gpt2-eval.py
+++ gpt2-eval.py
@@ -250,8 +250,8 @@ def main(argv=None):
     
     dataset_pd["prediction"] = [extract_answer(p) for p in full_predictions]
     exact_scores, f1_scores = score_data(dataset_pd)
-    dataset_pd["exact_score"] = exact_scores
-    dataset_pd["f1_score"] = f1_scores
+    dataset_pd["exact_score"] = exact_scores.values()
+    dataset_pd["f1_score"] = f1_scores.values()
 
     logging.info(f"Writing prediction and stats into {output_dir}")
     write_stats(dataset_pd, name, output_dir)
