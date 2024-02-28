a56f21008b68632622cdec224b94b069df0d0feb
Hannah Lin <hylin.hannah@gmail.com>
2024-02-28 05:42:41 +0000

    Add max seq length

--- gpt2-eval.py
+++ gpt2-eval.py
@@ -63,8 +63,9 @@ def get_predictions(prompts,
                     max_new_tokens=50,
                     use_cuda=True):
     input_ids = tokenizer(prompts,
-                          max_length=1000,
+                          max_length=1024,
                           padding="max_length", 
+                          truncation=True,
                           return_tensors="pt")
     if use_cuda:
         input_ids.to("cuda")
